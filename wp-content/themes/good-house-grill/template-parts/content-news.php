3dfa6e65bf5b33de859f55f90041f7d6afe96d32
Frank Simon <f.m.sim@t-online.de>
2018-04-28 20:42:59 +0200

    modified template-parts content-news.php and style.css

--- wp-content/themes/good-house-grill/template-parts/content-news.php
+++ wp-content/themes/good-house-grill/template-parts/content-news.php
@@ -1,1 +1,5 @@
-This is the news!
+<article <?php post_class('news-article'); ?>>
+  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  <time><?php echo get_the_date('M. j, Y'); ?></time>
+  <p><?php the_excerpt(); ?> <a href="<?php the_permalink(); ?>">Read More</a></p>
+</article>
